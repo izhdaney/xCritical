b77c7f043d2b8c2045c0b39603e3ee6cf8957adb
Ilya Zhdaney <fmgorod@gmail.com>
2021-07-07 12:18:51 +0300

    Initial Commit

---
+++ xCritical/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  xCritical
+//
+//  Created by Ilya Zhdaney on 07.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
